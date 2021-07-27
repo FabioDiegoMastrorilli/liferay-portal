41b14009f253fa4087589228efc11a097801141a
Luca Pellizzon <luca.pellizzon@liferay.com>
2021-07-27 09:50:34 +0200

    COMMERCE-5675 buildService

--- portal-impl/src/com/liferay/portal/service/http/OrganizationServiceSoap.java
+++ portal-impl/src/com/liferay/portal/service/http/OrganizationServiceSoap.java
@@ -222,6 +222,27 @@ public class OrganizationServiceSoap {
 		}
 	}
 
+	public static com.liferay.portal.kernel.model.UserSoap
+			addOrganizationUserByEmailAddress(
+				String emailAddress, long organizationId,
+				com.liferay.portal.kernel.service.ServiceContext serviceContext)
+		throws RemoteException {
+
+		try {
+			com.liferay.portal.kernel.model.User returnValue =
+				OrganizationServiceUtil.addOrganizationUserByEmailAddress(
+					emailAddress, organizationId, serviceContext);
+
+			return com.liferay.portal.kernel.model.UserSoap.toSoapModel(
+				returnValue);
+		}
+		catch (Exception exception) {
+			_log.error(exception, exception);
+
+			throw new RemoteException(exception.getMessage());
+		}
+	}
+
 	public static void addUserOrganizationByEmailAddress(
 			String emailAddress, long organizationId)
 		throws RemoteException {
